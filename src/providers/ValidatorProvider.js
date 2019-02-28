b9653679e9182f675445ecd20b15acc7fb447b2f
Sávio Serra <serra.santos.s@gmail.com>
2019-02-28 19:30:38 -0300

    ValidatorProvider

--- src/providers/ValidatorProvider.js
+++ src/providers/ValidatorProvider.js
@@ -1,1 +1,19 @@
-module.exports = {};
+const { ServiceProvider } = require('@adonisjs/fold');
+
+class ValidatorProvider extends ServiceProvider {
+  register() {
+    // Making sure those dependencies are booted
+    const iocDependencies = ['Validator', 'Database'];
+
+    iocDependencies.forEach(dep => this.app.use(dep));
+  }
+
+  boot() {
+    const Validator = use('Validator');
+    const cpf = require('../validators/cpf');
+
+    Validator.extend('cpf', cpf.bind(cpf));
+  }
+}
+
+module.exports = ValidatorProvider;
